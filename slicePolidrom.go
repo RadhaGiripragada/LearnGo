58d62aa1509076b038aacdf4c953e27b3ec9e1b0
Radha Giripragada <rgiripragada@gmail.com>
2024-05-02 13:26:31 -0700

    Create slicePolidrom.go
    a program that takes a slice of strings and checks if it forms a palindrome (reads the same forwards and backwards).

---
+++ slicePolidrom.go
@@ -0,0 +1,28 @@
+// a program that takes a slice of strings and checks if it forms a palindrome (reads the same forwards and backwards).
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+	var str string
+	str = "madam kayak radar test thesewords"
+	st, _ := strconv.Atoi(os.Args[1])
+	en, _ := strconv.Atoi(os.Args[2])
+	
+	poli := str[st:en+1]
+	fmt.Println("pil",poli)
+	leng := len(poli)
+	for i := 0; i < leng/2; i++ {
+		if poli[i] != poli[leng-i-1] {
+			fmt.Println("Not a polindrom")
+			return
+		}
+	}
+	fmt.Println("Polindrom")
+	return
+
+}
